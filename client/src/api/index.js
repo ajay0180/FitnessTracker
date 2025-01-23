a6a8b73e7e6974006c907f8bba2dfbe0eb526e94
ajay0180 <jangraajay489@gmail.com>
2025-01-23 22:54:15 +0530

    final phase 1.0

---
+++ client/src/api/index.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:8080/api";
+
+export const userSignUp = async (data) => {
+  try {
+    const response = await axios.post(`${baseURL}/user/signup`, data, {
+      headers: { "Content-Type": "application/json" },
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response?.data?.message || "Signup failed";
+  }
+};
+export const UserSignIn = async (data) =>
+  await axios.post(`${baseURL}/user/signin`, data, {
+    headers: { "Content-Type": "application/json" },
+  });
+
+export const getDashboardDetails = async (token) =>
+  axios.get(`${baseURL}/user/dashboard`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const getWorkouts = async (token, date) =>
+  await axios.get(`${baseURL}/user/workout?date=${date}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const addWorkout = async (token, data) =>
+  await axios.post(`${baseURL}/user/workout`, data, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
